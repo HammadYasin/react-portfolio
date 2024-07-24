1471fa0f3d6a4a41ae86c7fadd4d245e47c3a9fe
Hammad Yasin <hammadyasin817@gmail.com>
2024-07-24 17:12:09 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,146 @@
+import Me from './assets/images/Me.jpg';
+import MOHU from './assets/images/MOHU.jpg';
+import Meena from './assets/images/Meena.jpg';
+import CO from './assets/images/ML-MODELS.jpg';
+import JED from './assets/images/JeddahAmanah.jpg';
+const logotext = "Hammad";
+const meta = {
+    title: "Hammad Yasin",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Hammad Yasin",
+    animated: {
+        first: "I transform data into insights",
+        second: "I automate data pipelines",
+        third: "I build decision-making KPIs for key stakeholders",
+    },
+    description: "Welcome to my portfolio! I am a passionate data scientist with a strong foundation in front-end development. My journey began with creating responsive designs and intuitive UI layouts for web and mobile applications. Over time, I transitioned into the world of data science and analytics, where I now specialize in analyzing complex datasets, generating insightful statistical reports, and creating fully automated data pipelines. My expertise in machine learning enables me to drive data-driven decisions and deliver actionable insights that contribute to the success of forward-thinking organizations. Explore my projects and see how I apply my skills to solve real-world business cases",
+    your_img_url: Me,
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: `Welcome to my portfolio! I'm a dedicated data scientist with a strong background in front-end development. My career began with designing and developing responsive and intuitive UI layouts for web and mobile applications. This foundation in front-end development has given me a unique perspective on how to create user-friendly and impactful solutions.
+
+Transitioning into data science and analytics, I have honed my skills in analyzing complex datasets, generating insightful statistical reports, and creating fully automated data pipelines. My expertise extends to building decision-making KPIs that provide valuable insights for key stakeholders, driving data-driven decisions that contribute to the success of organizations.
+
+I am passionate about leveraging machine learning techniques to solve real-world business problems and uncover actionable insights. My goal is to help businesses harness the power of data to achieve their strategic objectives.
+
+Explore my projects to see how I apply my skills to create meaningful and impactful solutions.`,
+};
+const worktimeline = [
+    {
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Languages: C++, Java, Python, SQL, JavaScript (ES5 +6)",
+        value: 100,
+    },
+    {
+        name: "Tools: Power-BI, Anaconda Navigator, Excel, Apache Airflow",
+        value: 100,
+    },
+    {
+        name: "Client Scripting: jQuery, React, D3js, Webpack, Flask",
+        value: 100,
+    },
+    {
+        name: "Source Control: GitHub, Bitbucket",
+        value: 100,
+    },
+    {
+        name: "Native Mobile Development: Android Studio, X Code",
+        value: 100,
+    },
+    {
+        name: "Development Environments: VS Code",
+        value: 100,
+    },
+    {
+        name: "Web Frameworks: Flask",
+        value: 100,
+    },
+];
+
+const services = [
+    {
+        title: "Power BI Solutions",
+        description: `I create interactive and insightful dashboards using Power BI to help businesses visualize their data and make informed decisions. My expertise includes data modeling, report creation, and deploying solutions that provide actionable insights for key stakeholders.`,
+    },
+    {
+        title: "Mobile App Development",
+        description: `With a strong background in mobile app development, I design and develop user-friendly and responsive mobile applications. My focus is on creating intuitive UI layouts and seamless user experiences that meet the specific needs of your business.`,
+    },
+    {
+        title: "Web App Development",
+        description: `I specialize in building robust and scalable web applications that are both functional and aesthetically pleasing. By utilizing modern frameworks and technologies, I ensure that your web app is responsive, secure, and performs well across all devices.`,
+    },
+];
+
+const dataportfolio = [{
+        img: MOHU,
+        description: `Ministry of Hajj and Umrah Command Center`,
+        link: "#",
+    },
+    {
+        img: Meena,
+        description: "Tawuniya Command Center",
+        link: "#",
+    },
+    {
+        img: JED,
+        description: "Jeddah Amanah Command Center for Visual Pollution Room",
+        link: "#",
+    },
+    {
+        img: CO,
+        description: "Co-Creation HUB Machine Learning Models Website to Demonstrate Advanced Analytics USE-CASES",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hammadyasin817@gmail.com",
+    YOUR_FONE: "+966-592892844",
+    description: "I would love to connect and discuss how I can contribute to your next project. Feel free to reach out via email or through my LinkedIn profile. Let’s collaborate and make data work for you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_2302sdm",
+    YOUR_TEMPLATE_ID: "template_694649y",
+    YOUR_USER_ID: "yci4Bj9E1f1qoZYFt",
+};
+
+const socialprofils = {
+    github: "https://github.com/HammadYasin",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/hammad-yasin-324559218/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
